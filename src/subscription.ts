0ae6b27f91b3f03a4dfe460980c6c2ee4dff04d7
soags <17847282+soags@users.noreply.github.com>
2024-02-13 16:38:28 +0900

    Revert "テスト用に抽出条件を「フィード」に変更"
    This reverts commit 33e87f3cabfa6f2bb3adebb0a15018ebf8668dd7.

--- src/subscription.ts
+++ src/subscription.ts
@@ -9,14 +9,21 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
     if (!isCommit(evt)) return
     const ops = await getOpsByType(evt)
 
+    // This logs the text of every post off the firehose.
+    // Just for fun :)
+    // Delete before actually using
+    for (const post of ops.posts.creates) {
+      console.log(post.record.text)
+    }
+
     const postsToDelete = ops.posts.deletes.map((del) => del.uri)
     const postsToCreate = ops.posts.creates
       .filter((create) => {
-        return create.record.text.toLowerCase().includes('フィード')
+        // only alf-related posts
+        return create.record.text.toLowerCase().includes('alf')
       })
       .map((create) => {
-        console.log(create.record.text)
-
+        // map alf-related posts to a db row
         return {
           uri: create.uri,
           cid: create.cid,
